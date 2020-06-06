432fd56794a62fbac01524f1de4abb4afbc703fb
lpfazle <lpfarrow01@gmail.com>
2020-06-06 16:53:04 -0400

    css changes and testing

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import WelcomeMessage from './WelcomeMessage';
+import styled, {css} from "styled-components";
 
-function Index() {
-  return (
-      <div>Hello World</div>
-  );
+const MyButton = styled.button`
+  color: green;
+  border: 2px solid blue;
+  border-radius: 2px;
+  margin: 5px;
+  ${props => props.primary && css`
+    color: palevioletred;
+  `}
+`;
+
+const TomatoButton = styled(MyButton)`
+  color: tomato;
+  border-color: tomato;
+`;
+
+class App extends React.Component {
+  render() {
+    return (
+        <div>
+          <WelcomeMessage/>
+          <MyButton>Hello</MyButton>
+          <MyButton primary>Hello</MyButton>
+          <TomatoButton>Hello</TomatoButton>
+        </div>
+    );
+  }
 }
 
-export default Index;
+export default App;
