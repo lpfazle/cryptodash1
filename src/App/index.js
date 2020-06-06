5de941187a256a5a73c1f45312e2ff9d03a768bf
lpfazle <lpfarrow01@gmail.com>
2020-06-06 17:05:17 -0400

    update app layout

--- src/App/index.js
+++ src/App/index.js
@@ -1,32 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
-import styled, {css} from "styled-components";
+import AppLayout from "./AppLayout";
 
-const MyButton = styled.button`
-  color: green;
-  border: 2px solid blue;
-  border-radius: 2px;
-  margin: 5px;
-  ${props => props.primary && css`
-    color: palevioletred;
-  `}
-`;
-
-const TomatoButton = styled(MyButton)`
-  color: tomato;
-  border-color: tomato;
-`;
 
 class App extends React.Component {
   render() {
     return (
-        <div>
+        <AppLayout>
           <WelcomeMessage/>
-          <MyButton>Hello</MyButton>
-          <MyButton primary>Hello</MyButton>
-          <TomatoButton>Hello</TomatoButton>
-        </div>
+        </AppLayout>
     );
   }
 }
